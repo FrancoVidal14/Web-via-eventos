3659800bd6ec7c61b6f69d3d4bd9f94c6c90b456
FNVidal <franconicolasvidal@gmail.com>
2024-01-08 12:00:02 -0300

    Segundo commit

--- script.js
+++ script.js
@@ -2,22 +2,30 @@ document.addEventListener("DOMContentLoaded", function() {
     window.addEventListener('scroll', function() {
         let header = document.querySelector('header');
         let liHeader = document.querySelectorAll('li');
+        let contratarButton = document.querySelector('.contratar')
+        let logo = document.getElementById('logo')
         let scrollY = window.scrollY;
-    
+
+        console.log('scrollY:', scrollY);
         if (scrollY > 400) {
-            header.style.backgroundColor = '#c9b977';
+            console.log('Cambiando estilos y logo');
+            contratarButton.classList.add('contratarNegro');
+            header.style.backgroundColor = '#fff';
             liHeader.forEach(function(item){
                 item.style.color = 'black';
-        });
+            });
+            logo.src = "./Imagenes/Logo.png";
         } else {
+            contratarButton.classList.remove('contratarNegro');
             header.style.backgroundColor = 'transparent';
             liHeader.forEach(function(item){
                 item.style.color = 'white';
-        });
+            });
+            logo.src = "./Imagenes/Logo white.png";
         }
-    
+
         if(scrollY > 650) {
-            header.style.borderBottom = '2px solid gold'; 
+            header.style.borderBottom = '2px solid var(--butter)'; 
         }
         else{
             header.style.border = 'none';
